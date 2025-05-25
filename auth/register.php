9e2fbefebc0380823f3655ad609610c5177243c2
phanvantien2907 <vantienp530@gmail.com>
2025-05-25 20:10:05 +0700

    update 25/05/2025

--- auth/register.php
+++ auth/register.php
@@ -7,6 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
+    $role = 'user';
     $is_active = 1;
 
     if ($password != $confirm_password) {
@@ -25,14 +26,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     $hassed_password = password_hash("password", PASSWORD_BCRYPT);
-    $query = "INSERT INTO users (name, password, email, is_active) VALUES ('$name', '$hassed_password', '$email', 1)";
+    $query = "INSERT INTO users (name, password, email, role,  is_active) VALUES ('$name', '$hassed_password', '$email', '$role', 1)";
 
     if ($conn->query($query)) {
-        $_SESSION['success'] = "Đăng ký tài khoản $email thành công";
+        $_SESSION['toast_success'] = "Đăng ký tài khoản $email thành công";
         header("Location: register.php");
         exit();
     } else {
-        $_SESSION['error'] = "Đăng ký tài khoản $email thất bại";
+        $_SESSION['toast_error'] = "Đăng ký tài khoản $email thất bại";
         header("Location: register.php");
         exit();
     }
@@ -49,6 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Register</title>
 </head>
@@ -61,38 +63,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-        <title>Register</title>
+        <title>Đăng ký tài khoản</title>
     </head>
 
     <body>
-        <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
         <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <?php include '../admin/components/toast-message.php'; ?>
             <div class="sm:mx-auto sm:w-full sm:max-w-sm">
                 <img class="mx-auto h-10 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
                 <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign up to your account</h2>
             </div>
-
-            <?php if (isset($_SESSION['success'])): ?>
-                <div class="p-4 mb-4 text-sm text-green-700 bg-green-100 rounded-lg">
-                    <?= $_SESSION['success']; ?>
-                </div>
-                <?php unset($_SESSION['success']); ?>
-            <?php endif; ?>
-
-            <?php if (isset($_SESSION['error'])): ?>
-                <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg">
-                    <?= $_SESSION['error']; ?>
-                </div>
-                <?php unset($_SESSION['error']); ?>
-            <?php endif; ?>
-
             <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                 <form class="space-y-6" action="register.php" method="POST">
                     <div>
@@ -103,7 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                             <input type="name" name="name" id="name" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                         </div>
                     </div>
-                    
+
                     <div>
                         <div class="flex items-center justify-between">
                             <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
